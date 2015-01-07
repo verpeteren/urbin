3212cc09c0559146b2b3c032de830afe27e680e9
Peter Reijnders <Peter.Reijnders@verpeteren.nl>
2015-01-07 08:53:53 +0100

    cleanup of demo modules in the main routine.
    As these modules are compiled in, they are available anyway.
    As some do not have a onLoad, onReady and unUnload routine, it is not needed to call them in main.

--- src/main/main_server.cpp
+++ src/main/main_server.cpp
@@ -17,29 +17,16 @@ static void SignalHandler( const int signal ) {
 int main( int argc, const char ** argv ) {
 	cfg_t * config, * mainSection;
 	int fds;
-	struct module_t * webserverModule, *javascriptModule, *pgSqlclientModule;
-#if HAVE_MYSQL == 1
-	struct module_t * mySqlclientModule;
-#endif
+	struct module_t * javascriptModule;
 	struct {
 		unsigned int good:1;
 		unsigned int core:1;
-		unsigned int webserver:1;
-		unsigned int pgSqlclient:1;
-#if HAVE_MYSQL == 1
-		unsigned int mySqlclient:1;
-#endif
 		unsigned int javascript:1;
 		} cleanUp;
 
 	memset( &cleanUp, 0, sizeof( cleanUp ) );
 	core = NULL;
-	webserverModule = NULL;
 	javascriptModule = NULL;
-	pgSqlclientModule = NULL;
-#if HAVE_MYSQL == 1
-	mySqlclientModule = NULL;
-#endif
 	cleanUp.good = ( ( config = ProcessCommandline( argc, argv ) ) != NULL );
 	fds = PR_CFG_CORE_MAX_FDS;
 	if ( cleanUp.good ) {
@@ -52,29 +39,6 @@ int main( int argc, const char ** argv ) {
 	}
 	if ( cleanUp.good ) {
 		cleanUp.core = 1;
-		cleanUp.good = ( ( webserverModule = Module_New( "webserver", NULL, NULL, NULL, NULL, NULL ) ) != NULL );
-	}
-	if ( cleanUp.good ) {
-		cleanUp.good = ( Core_AddModule( core, webserverModule ) ) ? 1 : 0;
-	}
-	if ( cleanUp.good ) {
-		cleanUp.webserver = 1;
-		cleanUp.good = ( ( pgSqlclientModule = Module_New( "postgresql", NULL, NULL, NULL, NULL, NULL ) ) != NULL );
-	}
-	if ( cleanUp.good ) {
-		cleanUp.good = ( Core_AddModule( core, pgSqlclientModule ) ) ? 1 : 0;
-	}
-	if ( cleanUp.good ) {
-		cleanUp.pgSqlclient = 1;
-#if HAVE_MYSQL == 1
-		cleanUp.good = ( ( mySqlclientModule = Module_New( "mysqlclient", NULL, NULL, NULL, NULL, NULL ) ) != NULL );
-	}
-	if ( cleanUp.good ) {
-		cleanUp.good = ( Core_AddModule( core, mySqlclientModule ) ) ? 1 : 0;
-	}
-	if ( cleanUp.good ) {
-		cleanUp.mySqlclient = 1;
-#endif
 		cleanUp.good = ( ( javascriptModule = Module_New( "javascript", JavascriptModule_Load, JavascriptModule_Ready, JavascriptModule_Unload, NULL, NULL ) ) != NULL );
 	}
 	if ( cleanUp.good ) {
@@ -89,17 +53,6 @@ int main( int argc, const char ** argv ) {
 		Core_Delete( core );
 	}
 	if ( ! cleanUp.good ) {
-		if ( cleanUp.webserver ) {
-			Core_DelModule( core, webserverModule );
-		}
-		if ( cleanUp.pgSqlclient) {
-			Core_DelModule( core, pgSqlclientModule );
-		}
-#if HAVE_MYSQL == 1
-		if ( cleanUp.mySqlclient) {
-			Core_DelModule( core, mySqlclientModule );
-		}
-#endif
 		if ( cleanUp.javascript ) {
 			Core_DelModule( core, javascriptModule );
 		}
